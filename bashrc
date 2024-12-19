8ab9dc03214dc8f89f619fca060b05f891ead4e4
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2024-12-19 18:28:39 +0100

    new start

---
+++ bashrc
@@ -0,0 +1,94 @@
+#
+# ~/.bashrc
+#
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+alias leetcode='g++ -Wall -Wextra -g -fsanitize=address'
+
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+export PS1="\w\\$ "
+
+alias ga='git add'
+alias gc='git commit'
+alias gd='git diff'
+alias gds='git diff --staged'
+alias gs='git status'
+alias gp='git push'
+alias gpu='git pull'
+alias glo='git log --oneline'
+alias :r="source ~/.bashrc"
+alias bashrc='nvim ~/.bashrc'
+alias de='setxkbmap de'
+alias us='setxkbmap us'
+alias xclip='xclip -selection c'
+
+alias archive='time yt-dlp --yes-playlist -R infinite --retry-sleep=exp=1 --restrict-filenames --write-description --write-info-json --write-thumbnail -S "ext"'
+
+alias sshbig='ssh cloud@141.5.106.221'
+alias sshdaemon='ssh cloud@141.5.108.64'
+
+function cd() { builtin cd -- "$@" && { [ "$PS1" = "" ] || ls -hrt --color; }; }
+
+function setup_venv() {
+    if [[ ! -d venv ]]; then 
+        echo "creating venv"
+        python3 -m venv venv
+        if [[ ! -f ./.gitignore ]] || ! grep -q "^venv/$" ./.gitignore; then
+            echo "venv/" >> ./.gitignore
+        fi
+        if [[ -f ./requirements.txt ]]; then 
+            echo "requirements.txt found, installing"
+            ./venv/bin/pip install -r ./requirements.txt
+        fi
+    fi
+    source ./venv/bin/activate
+}
+alias venv=setup_venv
+
+setup_work() {
+    tmux new-session -d -s work
+
+    tmux send-keys -t work:0.0 "cd ~/code/forevercode/foreverbusy/" C-m
+    tmux send-keys -t work:0.0 "source ./venv/bin/activate" C-m
+    tmux send-keys -t work:0.0 "clear" C-m
+    tmux send-keys -t work:0.0 "python3 summarize.py" C-m
+    tmux send-keys -t work:0.0 "python3 track.py "
+
+    tmux split-window -h -t work:0
+    tmux send-keys -t work:0.1 "cd ~/code/lquentin/time_tracking/" C-m
+    tmux send-keys -t work:0.1 "nvim timetracking.md" C-m
+
+    tmux select-pane -t work:0.0
+    tmux attach-session -t work
+}
+alias work=setup_work
+
+
+if [[ "$(hostname)" == "omniblech" ]]; then
+    install_date="2024-11-09"
+    current_date=$(date +%Y-%m-%d)
+    days_since_install=$(($(($(date -d "$current_date" "+%s") - $(date -d "$install_date" "+%s"))) / 86400))
+    echo "Installed at 21.10.2024 ($days_since_install days)"
+fi
+
+
+# saxon stuff
+export SAXONPATH=/home/$USER/pkg
+export SAXON12CLASSPATH=$SAXONPATH/saxon-he-12.5.jar:$SAXONPATH/lib/xmlresolver-5.2.2.jar:$SAXONPATH/lib/xmlresolver-5.2.2-data.jar
+alias saxonXQ='java -cp $SAXON12CLASSPATH net.sf.saxon.Query'
+alias saxonXSL='java -cp $SAXON12CLASSPATH net.sf.saxon.Transform'
+alias saxonValid='java -cp $SAXON12CLASSPATH net.sf.saxon.Query -qs:. -dtd:on '
+
+export PATH=/home/$USER/.local/bin:/home/$USER/pkg:/home/$USER/.cargo/bin:$PATH
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+
+[ -f "/home/$(whoami)/.ghcup/env" ] && . "/home/$(whoami)/.ghcup/env" # ghcup-env
+. "$HOME/.cargo/env"
---
+++ nvim/init.lua
@@ -0,0 +1,258 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- install lazy
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+  {'numToStr/Comment.nvim', opts = {} },
+  {'akinsho/bufferline.nvim', version = "*"},
+  {'kyazdani42/nvim-tree.lua'},
+  {'ntpeters/vim-better-whitespace'},
+  -- {"yutkat/confirm-quit.nvim", event = "CmdlineEnter", opts = {}}, -- Only useful if used w/in neovide
+
+  -- Themes
+  {"polirritmico/monokai-nightasty.nvim"},
+  {"navarasu/onedark.nvim"},
+  {"projekt0n/github-nvim-theme"},
+  {"ewilazarus/preto"},
+
+
+  -- lsp stuff
+  {'neovim/nvim-lspconfig'},
+  {'williamboman/mason.nvim'},
+  {'williamboman/mason-lspconfig.nvim'},
+  {'hrsh7th/nvim-cmp'},
+  {'hrsh7th/cmp-nvim-lsp'}
+}, {})
+
+-- general
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.bo.softtabstop = 2
+
+-- Set highlight on search
+vim.o.hlsearch = true
+
+-- Explicitly deactivate the cursorline
+vim.o.cursorline = false
+
+-- Minimal number of lines to keep above and below cursor
+vim.o.scrolloff = 4
+vim.o.sidescrolloff = 4
+
+-- Make absolute relative line numbers default
+vim.wo.number = true
+--vim.wo.relativenumber = true
+vim.wo.relativenumber = false
+
+-- Disable mouse mode
+vim.o.mouse = ''
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+-- Note: On Wayland, this needs `wl-copy` (debian package: `wl-clipboard`)
+vim.o.clipboard = 'unnamedplus'
+
+-- Case-insensitive searching UNLESS \C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- remove the sign column (left of the numbers)
+vim.wo.signcolumn = 'auto'
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
+
+-- NOTE: You should make sure your terminal supports this
+vim.o.termguicolors = true
+
+-- Color Column for 80 chars
+vim.o.colorcolumn = "80"
+
+vim.api.nvim_create_user_command(
+  'Bd',
+  function()
+    vim.cmd('bp')
+    vim.cmd('bd #')
+  end,
+  { desc = 'Go to previous buffer and delete the current one' }
+)
+
+-- color scheme
+
+-- Koehler
+vim.cmd.colorscheme("koehler")
+
+-- Monokai
+-- vim.opt.background = "dark" -- default to dark or light style
+-- vim.cmd.colorscheme("monokai-nightasty")
+
+-- Onedark
+-- require('onedark').setup {
+--     style = 'darker'
+-- }
+-- require('onedark').load()
+
+-- github
+-- vim.cmd('colorscheme github_dark_default')
+
+-- preto
+-- vim.cmd('colorscheme preto')
+
+
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Escape from terminal on any layout
+vim.keymap.set("t", "<S-esc>", "<C-\\><C-n>")
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+    callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
+vim.cmd([[
+  " Automatically resize splits after window size change
+  " https://coderwall.com/p/it7wka/vim-resplit-after-window-size-change
+  augroup Misc
+    autocmd!
+    autocmd VimResized * exe "normal! \<c-w>="
+  augroup END
+]])
+
+
+-- bufferline.nvim
+require("bufferline").setup{
+  options = {
+    show_buffer_icons = false, -- disable filetype icons for buffers
+    show_buffer_close_icons = false,
+    show_close_icon = false,
+    show_tab_indicators = false
+  }
+}
+vim.keymap.set("n", "<S-l>", ":bnext<CR>", {noremap = true})
+vim.keymap.set("n", "<S-h>", ":bprevious<CR>", {noremap = true})
+
+-- nvim-tree.lua
+require("nvim-tree").setup({
+  renderer = {
+    icons = {
+      web_devicons = {
+        file = {
+          enable = false,
+          color = false,
+        },
+        folder = {
+          enable = false,
+          color = false,
+        }
+      },
+      glyphs = {
+        default = "-", -- is a file
+        symlink = "-", -- is a symlink
+        bookmark = "b", -- is a bookmark?
+        modified = "●",
+        folder = {
+          arrow_closed = "-", -- collapsed
+          arrow_open = "-", -- not collapsed
+          default = "/", -- is a normal folder
+          open = "/", -- is a non-collapsed folder
+          empty = "/", -- ...
+          empty_open = "/",
+          symlink = "/",
+          symlink_open = "/",
+        }
+      }
+    }
+  }
+})
+vim.keymap.set("n", "<leader>f", ":NvimTreeToggle<cr>", {noremap = true })
+
+-- LSP setup from https://lsp-zero.netlify.app/v3.x/blog/you-might-not-need-lsp-zero.html
+-- note: diagnostics are not exclusive to lsp servers
+-- so these can be global keybindings
+vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>') 
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function(event)
+    local opts = {buffer = event.buf}
+
+    -- these will be buffer-local keybindings
+    -- because they only work if you have an active language server
+
+    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+    vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+  end
+})
+
+local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+local default_setup = function(server)
+  require('lspconfig')[server].setup({
+    capabilities = lsp_capabilities,
+  })
+end
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {"clangd", "rust_analyzer"},
+  handlers = {
+    default_setup,
+  },
+})
+
+local cmp = require('cmp')
+cmp.setup({
+  sources = {
+    {name = 'nvim_lsp'},
+  },
+  mapping = cmp.mapping.preset.insert({
+    -- Enter key confirms completion item
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+    -- Ctrl + space triggers completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+  }),
+})
+
+-- vim: ts=2 sts=2 sw=2 et
