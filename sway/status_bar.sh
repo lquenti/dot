749786824b85658d35b84ce2e51d03f0f59f52f1
Lars Quentin <lars@lquenti.de>
2025-05-30 16:19:08 +0200

    update bar

--- sway/status_bar.sh
+++ sway/status_bar.sh
@@ -1,3 +1,4 @@
-date=$(date +'%d.%m.%Y %H:%M:%S')
-bat=$(cat /sys/class/power_supply/BAT0/capacity)
-echo "$bat% | $date"
+date=$(date +'%d.%m.%Y %H:%M')
+bat1=$(cat /sys/class/power_supply/BAT0/capacity)
+bat2=$(cat /sys/class/power_supply/BAT0/status)
+echo "$bat1% ($bat2) | $date"
