dd9b36fb98127f7eec265ba0b5b81629a0cfc3f5
Lars Quentin <lars@lquenti.de>
2025-07-18 23:02:58 +0200

    cpu temp

--- sway/status_bar.sh
+++ sway/status_bar.sh
@@ -1,4 +1,5 @@
-date=$(date +'%d.%m.%Y %H:%M')
+temp=$(sensors -j | jq '(.["coretemp-isa-0000"]["Core 0"].temp2_input * 10 | floor) / 10')
 bat1=$(cat /sys/class/power_supply/BAT0/capacity)
 bat2=$(cat /sys/class/power_supply/BAT0/status)
-echo "$bat1% ($bat2) | $date"
+date=$(date +'%a. %d.%m.%Y %H:%M')
+echo "${temp}°C | $bat1% ($bat2) | $date"
